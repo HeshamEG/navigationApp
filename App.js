e6e06d8df3d3a7a1abfba8f6aebbdf3ec6b288a6
HeshamEG <heshamsalama1212901022@gmail.com>
2018-02-26 21:29:45 +0200

    d

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { Navigation } from "react-native-navigation";
 import AuthScreen from "./src/screens/Auth/Auth";
 import SharePlace from "./src/screens/SharePlace/SharePlace";
 import FindPlace from "./src/screens/FindPlace/FindPlace";
-
+import {Prvider} from "react-redux"
+import configureStore from './src/store/configureStore'
+const store =configureStore();
 // Register Screens
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("awesome-places.SharePlace", () => SharePlace);
-Navigation.registerComponent("awesome-places.FindPlace", () => FindPlace);
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen,store,Prvider);
+Navigation.registerComponent("awesome-places.SharePlace", () => SharePlace,store,Prvider);
+Navigation.registerComponent("awesome-places.FindPlace", () => FindPlace,store,Prvider);
 
 
 // Start a App
